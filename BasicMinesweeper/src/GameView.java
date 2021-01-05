d3f7cb66166d2dbfc3f1a0c881e5eacb5d29743f
s204496 <s204496@student.dtu.dk>
2021-01-05 15:27:23 +0100

    01/05-15:30 Kevin og Anders

--- BasicMinesweeper/src/GameView.java
+++ BasicMinesweeper/src/GameView.java
@@ -1,4 +1,37 @@
 
-public class GameView {
+	import javafx.application.Application;
+	import javafx.event.ActionEvent;
+	import javafx.event.EventHandler;
+	import javafx.scene.Scene;
+	import javafx.scene.control.Button;
+	import javafx.scene.layout.GridPane;
+	import javafx.scene.layout.StackPane;
+	import javafx.stage.Stage;	
 
+public class GameView {
+	
+	public GridPane gridPane;
+	public Button buttons;
+	
+	public GameView(int nsize, int msize) {
+		for(int i=0; i<nsize; i++) {
+			for(int j=0; j<msize; j++) {
+				gridPane.add(new Button(), i, j);
+			}
+		}
+	}
+	
+	public void showScene() {
+		
+	}
+	
+	public void buttonClickAction() {
+		int y = GridPane.getRowIndex(buttons);
+		int x = GridPane.getColumnIndex(buttons);		
+	}
+	
+	/*public void seteventhandler(eventhandler) {
+		button.setOnAction((eventhandler));
+}
+	*/
 }
--- BasicMinesweeper/src/Main.java
+++ BasicMinesweeper/src/Main.java
@@ -4,7 +4,31 @@ public class Main {
 	public static void main(String[] args) {
 		GameModel gameModel = new GameModel(7, 5, 5);
 		System.out.print(gameModel.printBoard());
-
 	}
 
 }
+
+/*
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+}
+*/
