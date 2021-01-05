c39b0513726e4a6f6c0815ff3c3228381079cb47
s204472 <s204472@student.dtu.dk>
2021-01-05 13:20:14 +0100

    Model, Constructor, tostring

--- BasicMinesweeper/src/GameModel.java
+++ BasicMinesweeper/src/GameModel.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+
+
 
 public class GameModel {
 	private int n;
@@ -6,27 +9,51 @@ public class GameModel {
     private boolean[][] mines;
     private GameObjects[][] currentBoard;
     private GameObjects[][] finalBoard;
-    
+     
     
     public GameModel(int n, int m, int mineCount) {
     	this.n = n;
     	this.m = m;
     	this.mineCount = mineCount;
     	
-    	currentBoard = new GameObjects[n][m];
-    	finalBoard = new GameObjects[n][m];
+    	this.currentBoard = new GameObjects[n][m];
+    	this.finalBoard = new GameObjects[n][m];
+    	this.mines = new boolean[n][m];
     	
-    	mines = fillMines(mineCount);
+    	this.mines = fillMines(mineCount);
+    	
     	
     	
     }
 	
-	private GameObjects[][] fillMines(int mineCount) {
+	private boolean[][] fillMines(int mineCount) {
+		Random rand = new Random();
+		int mineIndex = 0;
+		while (mineIndex < mineCount) {
+			int x = rand.nextInt(n);
+			int y = rand.nextInt(m);
+			if (!mines[x][y]) {
+				mines[x][y] = true;
+				mineIndex++;
+			}
+			
+		}
+		 
 		
 		
 		return mines;
 	}
     
+	public String printBoard() {
+		String s = "";
+		for (int i = 0; i < mines.length; i++) {
+			for (int j = 0; j < mines[i].length; j++) {
+				s += mines[i][j] ? "X\t" : "O\t";
+			}
+			s += "\n";
+		}
+		return s;
+	}
     
 	
 	
