a6fc1625a40fd8a2e209a267bc72f7781d467631
ande310g <andersanders@live.dk>
2021-01-07 12:59:18 +0100

    Commit before merge

--- BasicMinesweeper/src/Main.java
+++ BasicMinesweeper/src/Main.java
@@ -1,6 +1,5 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
@@ -10,49 +9,46 @@ public class Main extends Application {
 	private static int xSize;
 	private static int ySize;
 	private static int mines;
-	
-	
-    public static void main(String[] args) {
+
+	public static void main(String[] args) {
     	 
     	// Handling arguments from the command line.
-    	if (args.length != 3) {
-             System.out.println("Not correct number of arguments, please only enter 3"); 
-    	} else {
-             xSize = Integer.parseInt(args[0]);
-             ySize = Integer.parseInt(args[1]);
-             mines = Integer.parseInt(args[2]);
-             if (xSize >= 100 && xSize <= 4 && ySize >= 100 && ySize <= 4 && mines >= 2000 && mines <= 5 && mines > xSize*ySize) {
-            	 throw new IllegalArgumentException("Illegal size arguments given");
-             }
-         }
-    	 launch(args); //lauches the method start
+		try {
+			if (args.length != 3) {
+				System.out.println("Not correct number of arguments, please only enter 3"); 
+			} else {
+				xSize = Integer.parseInt(args[0]);
+				ySize = Integer.parseInt(args[1]);
+				mines = Integer.parseInt(args[2]);
+				if (xSize >= 100 && xSize <= 4 && ySize >= 100 && ySize <= 4 && mines >= 2000 && mines <= 5 && mines > xSize*ySize) {
+					throw new IllegalArgumentException("Illegal size arguments given");
+				}
+			}
+			launch(args); //lauches the method start
+		} catch(Exception e) {
+			throw new IllegalArgumentException("Please enter 3 integers.");
+		}
     }
 
-    public void start(Stage primaryStage) throws Exception{
-    	
-        GameModel gameModel = new GameModel(xSize, ySize, mines);
-        
-        
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("view.fxml"));
-        loader.setControllerFactory(new Callback<Class<?>, Object>() {
-            @Override
-            public Object call(Class<?> aClass) {
-                return new Controller(gameModel);
-            }
-        });
-    	
-    	
-        GridPane root = (GridPane) loader.load();
-        primaryStage.setTitle("Basic-Minesweeper");
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-        
-        
-       
+	public void start(Stage primaryStage) throws Exception {
 
-    }
+		GameModel gameModel = new GameModel(xSize, ySize, mines);
 
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("view.fxml"));
+		loader.setControllerFactory(new Callback<Class<?>, Object>() {
+			@Override
+			public Object call(Class<?> aClass) {
+				return new Controller(gameModel);
+			}
+		});
+
+		GridPane root = (GridPane) loader.load();
+		primaryStage.setTitle("Basic-Minesweeper");
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+	}
 
 }
