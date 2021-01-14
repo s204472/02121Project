c7eee61f703231b9bbb91b22402bc07688e30d41
s204472 <s204472@student.dtu.dk>
2021-01-14 08:35:13 +0100

    Libraries

--- AdvancedMinesweeper/src/DatabaseConfig.java
+++ AdvancedMinesweeper/src/DatabaseConfig.java
@@ -3,13 +3,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConfig {
-	private String MySQLURL = "jdbc:mysql://localhost:3306/minesweepertest";
-	private String databseUserName = "root";
+	private String MySQLURL = "jdbc:mysql://127.0.0.1:3306/minesweepertest";
+	private String databaseUserName = "root";
 	private String databasePassword = "";
 	private Connection con;
 	
 	public DatabaseConfig() throws SQLException {
-		con = DriverManager.getConnection(MySQLURL, databseUserName, databasePassword);
+		con = DriverManager.getConnection(MySQLURL, databaseUserName, databasePassword);
 	}
 	public Connection getCon() {
 		return con;
