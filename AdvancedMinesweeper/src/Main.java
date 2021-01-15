d7e6f1d81f4e203d7436a6b3994dafb3b0f664c2
Kevin Moore <hello@moorekevin.dk>
2021-01-15 11:29:38 +0100

    fixed 3bv score

--- AdvancedMinesweeper/src/Main.java
+++ AdvancedMinesweeper/src/Main.java
@@ -4,7 +4,6 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
 
 	public static void main(String[] args) {
--- AdvancedMinesweeper/src/ScoreModel.java
+++ AdvancedMinesweeper/src/ScoreModel.java
@@ -1,170 +1,88 @@
 
 public class ScoreModel {
-	private int score3BV;
+	private int value3BV;
 	private int secondsPassed;
-	private int[][] checkedZeros;
+	private boolean[][] checkedZeros;
+	
+	private boolean[][] zerosToCheck;
 
 	public ScoreModel (GameObjects[][] solutionBoard) {
-		// Calculates the optimal solution using 3BV
-		int amountOfClicks = 1;
-		checkedZeros = new int[solutionBoard.length][solutionBoard[0].length];
+		
+		// Calculates the most optimal solution using 3BV
+		checkedZeros = new boolean[solutionBoard.length][solutionBoard[0].length];
+		zerosToCheck = new boolean[solutionBoard.length][solutionBoard[0].length];
 		
 		for (int i = 0; i < solutionBoard.length; i++) {
 			for (int j = 0; j < solutionBoard[i].length; j++) {
 				
-				if (checkedZeros[i][j] == 0) {
-					if (solutionBoard[i][j] instanceof Zero) {
-						amountOfClicks++;
-						hasBlankNeighbours(solutionBoard, i, j, true);
-					}
+				// Checks how many zeros needed to be pressed
+				if (solutionBoard[i][j] instanceof Zero && !checkedZeros[i][j]) {
+					checkZeroSequence(solutionBoard, i,j);
+					value3BV++;
 				}
 				
 				if (solutionBoard[i][j] instanceof Number) {
-					if (!hasBlankNeighbours(solutionBoard, i, j, false)) {
-						amountOfClicks++;
+					if (!canBeDiscoveredByBlank(solutionBoard, i, j)) {
+						value3BV++;
 					} 
 				}
 				
 			}
 		}
-		System.out.println("Amount of clicks = " + amountOfClicks);
+		
+		System.out.println("Amount of clicks " + value3BV);
 	}
 	
-	public boolean hasBlankNeighbours(GameObjects[][] solutionBoard, int x, int y, boolean checkZero) {
-		if (x-1 >= 0) {
-			if (solutionBoard[x-1][y] instanceof Zero) {
+	// Checks whether the square can be discovered by the blank square sequence.
+	// Only used for numbered squares (Not zeros)
+	public boolean canBeDiscoveredByBlank(GameObjects[][] solutionBoard, int x, int y) {
+		for (int i = x - 1; i <= x + 1; i++) {
+			for (int j = y - 1; j <= y + 1; j++) {
 				
-				if(checkZero && checkedZeros[x-1][y] == 0) {
-					checkedZeros[x-1][y]=1;
-					hasBlankNeighbours(solutionBoard,x-1,y,true);
+				// If any square next to the searched square is a zero the method ends with 'true'
+				if ((i != x || j != y) && i >= 0 && i < solutionBoard.length && j >= 0
+						&& j < solutionBoard[i].length && solutionBoard[i][j] instanceof Zero) {
+					
+					return true;
 				}
 				
-				return true;
 			}
-			if (y-1 >= 0 && solutionBoard[x-1][y-1] instanceof Zero) {
-				
-				if(checkZero && checkedZeros[x-1][y-1] == 0) {
-					checkedZeros[x-1][y-1]=1;
-					hasBlankNeighbours(solutionBoard,x-1,y-1,true);
-				}
-				
-				return true;
-			}
-			if (y+1 < solutionBoard[x-1].length && solutionBoard[x-1][y+1] instanceof Zero ) {
-				
-				if(checkZero && checkedZeros[x-1][y+1] == 0) {
-					checkedZeros[x-1][y+1]=1;
-					hasBlankNeighbours(solutionBoard,x-1,y+1,true);
-				}
-				
-				return true;
-			}
-		}
-		
-		
-		if (x+1 < solutionBoard.length) {
-			if (solutionBoard[x+1][y] instanceof Zero) {
-				
-				if(checkZero && checkedZeros[x+1][y] == 0) {
-					checkedZeros[x+1][y]=1;
-					hasBlankNeighbours(solutionBoard,x+1,y,true);
-				}
-				
-				return true;
-			}
-			if (y-1 >= 0 && solutionBoard[x+1][y-1] instanceof Zero) {
-				
-				if(checkZero && checkedZeros[x+1][y-1] == 0) {
-					checkedZeros[x+1][y-1]=1;
-					hasBlankNeighbours(solutionBoard,x+1,y-1,true);
-				}
-				
-				return true;
-			}
-			if (y+1 < solutionBoard[x+1].length && solutionBoard[x+1][y+1] instanceof Zero ) {
-				
-				if(checkZero && checkedZeros[x+1][y+1] == 0) {
-					checkedZeros[x+1][y+1]=1;
-					hasBlankNeighbours(solutionBoard,x+1,y+1,true);
-				}
-				
-				return true;
-			}
-		}
-		
-		if (y-1 >= 0 && solutionBoard[x][y-1] instanceof Zero) {
-			
-			if(checkZero && checkedZeros[x][y-1] == 0) {
-				checkedZeros[x][y-1]=1;
-				hasBlankNeighbours(solutionBoard,x,y-1,true);
-			}
-			
-			return true;
-		}
-		
-		if (y+1 < solutionBoard[x].length && solutionBoard[x][y+1] instanceof Zero ) {
-			
-			if(checkZero && checkedZeros[x][y+1] == 0) {
-				checkedZeros[x][y+1]=1;
-				hasBlankNeighbours(solutionBoard,x,y+1,true);
-			}
-			
-			return true;
 		}
 		
 		
 		return false;
 	}
-	/* TIL DEBUGGING MÅ SLETTES
-	public boolean hasSpecialBlankNeighbours(GameObjects[][] solutionBoard, int x, int y) {
-		if (x-1 >= 0) {
-			System.out.println("1");
-			if (solutionBoard[x-1][y] instanceof Zero) {
-				System.out.println("2");
-				return true;
-			}
-			if (y-1 >= 0 && solutionBoard[x-1][y-1] instanceof Zero) {
-				System.out.println("3");
-				return true;
-			}
-			if (y+1 < solutionBoard.length && solutionBoard[x-1][y+1] instanceof Zero ) {
-				System.out.println("4");
-				return true;
-			}
-		}
-		
-		
-		if (x+1 < solutionBoard.length) {
-			System.out.println("5");
-			if (solutionBoard[x+1][y] instanceof Zero) {
-				System.out.println("6");
-				return true;
-			}
-			if (y-1 >= 0 && solutionBoard[x+1][y-1] instanceof Zero) {
-				System.out.println("7");
-				return true;
-			}
-			if (y+1 < solutionBoard.length && solutionBoard[x+1][y+1] instanceof Zero ) {
-				System.out.println("8");
-				return true;
+	
+	public void checkZeroSequence(GameObjects[][] solutionBoard, int x, int y) {		
+		for (int i = x - 1; i <= x + 1; i++) {
+			for (int j = y - 1; j <= y + 1; j++) {
+				
+				// Makes sure always to be in bound of Array
+				if ((i != x || j != y) && i >= 0 && i < solutionBoard.length && j >= 0
+						&& j < solutionBoard[i].length) {
+
+					if (solutionBoard[i][j] instanceof Zero && !checkedZeros[i][j]) {
+						zerosToCheck[i][j] = true;
+					}
+					
+					
+					
+				}
 			}
 		}
+		checkedZeros[x][y] = true;
+		zerosToCheck[x][y] = false;
 		
-		if (y-1 >= 0 && solutionBoard[x][y-1] instanceof Zero) {
-			System.out.println("9");
-			return true;
+		
+		// Makes sure that all the zero squares in the sequence will be checked
+		for (int i = 0; i < zerosToCheck.length; i++) {
+			for (int j = 0; j < zerosToCheck[i].length; j++) {
+				if (zerosToCheck[i][j]) {
+					checkZeroSequence(solutionBoard, i, j);
+				}
+			}
 		}
-		
-		if (y+1 < solutionBoard.length && solutionBoard[x][y+1] instanceof Zero ) {
-			System.out.println("10");
-			
-			return true;
-		}
-		
-		
-		return false;
 	}
-	*/
 	
 	// Timing part of score
 	public void incSeconds() {
@@ -181,7 +99,7 @@ public class ScoreModel {
 	}
 	
 	public String getScore() {
-		int endScore = 0 - secondsPassed;
+		int endScore = value3BV*6-secondsPassed;
 		if (endScore < 0) {
 			endScore = 0;
 		}
